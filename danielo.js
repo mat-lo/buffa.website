0073914249c2a10f7b375671709dd5bc8aed9885
m <write@matlo.me>
2018-07-20 10:05:39 +0100

    faster mobile

--- danielo.js
+++ danielo.js
@@ -637,10 +637,10 @@ function handleTouchMove(evt) {
     } else {
         if ( yDiff > 0 ) {
             /* up swipe */ 
-		acc.x+= 0.04;
+		acc.x+= 0.07;
         } else { 
             /* down swipe */
-		acc.x-= 0.04;
+		acc.x-= 0.07;
         }                                                                 
     }
     /* reset values */
