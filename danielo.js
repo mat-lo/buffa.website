e255514dd7a38fca2217ded7fd1c895fdd2f6239
m <write@matlo.me>
2018-07-20 09:45:25 +0100

    mobile friendly

--- danielo.js
+++ danielo.js
@@ -605,3 +605,45 @@ if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
 		   // mesh.rotation.y = mesh.rotation.y-0.15; 		    
 		  }		  
 		}
+
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;                                                        
+var yDown = null;                                                        
+
+function handleTouchStart(evt) {                                         
+    xDown = evt.touches[0].clientX;                                      
+    yDown = evt.touches[0].clientY;                                      
+};                                                
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            /* left swipe */ 
+        } else {
+            /* right swipe */
+        }                       
+    } else {
+        if ( yDiff > 0 ) {
+            /* up swipe */ 
+		acc.x+= 0.01;
+        } else { 
+            /* down swipe */
+		acc.x-= 0.01;
+        }                                                                 
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
